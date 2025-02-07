8827b0cdbecf37779c4d0928b66e6115eae112de
micah <micahtid@gmail.com>
2025-02-07 07:55:05 +0700

    rudimentary Firebase hooks!

---
+++ providers/UserProvider.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+import { DocumentData } from "firebase/firestore";
+import { Auth, User } from "firebase/auth";
+
+import { initializeFirebase, getUserAuth } from "@/utils/databaseFunctions";
+import { getUser } from "@/utils/userFunctions";
+
+// Context Type Definition
+type UserContextType = {
+  user: User | null | undefined;
+  userData: DocumentData | null | undefined;
+};
+
+// Creating User Context
+export const UserContext = createContext<UserContextType | undefined>(
+  undefined
+);
+
+export interface Props {
+  [key: string]: any;
+}
+
+export const UserContextProvider = ({ children, ...props }: Props) => {
+  const auth = getUserAuth(true);
+  const [user, setUser] = useState<User | null | undefined>(undefined);
+  const [userData, setUserData] = useState<DocumentData | null | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (firebaseUser) => {
+      setUser(firebaseUser);
+      setUserData(firebaseUser ? await getUser(firebaseUser.uid) : null);
+    });
+    return () => unsubscribe();
+  }, [auth]);
+
+  return (
+    <UserContext.Provider value={{ user, userData }} {...props}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+// Hook For Accessing User Context
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) throw new Error("useUser must be used within a UserContextProvider");
+  return context;
+};
